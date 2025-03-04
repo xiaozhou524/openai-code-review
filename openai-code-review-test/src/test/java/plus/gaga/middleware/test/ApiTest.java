be1b792f852343b96726c2f0474db0ff4cad693e
小舟12138 <1657248509@qq.com>
2025-03-04 17:02:52 +0800

    feat: 工程重构

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("uuu"));
+        System.out.println(Integer.parseInt("qqq"));
     }
 
 }
